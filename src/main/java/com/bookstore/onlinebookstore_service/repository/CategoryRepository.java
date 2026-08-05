42cb78d03d1b3e7e1fc693e97b790fd5a73913d9
jyoti92173 <Jyoti53913@gmail.com>
2026-08-05 22:16:49 +0530

    Create CategoryRepository using JpaRepository

---
+++ src/main/java/com/bookstore/onlinebookstore_service/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.bookstore.onlinebookstore_service.repository;
+
+import com.bookstore.onlinebookstore_service.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
