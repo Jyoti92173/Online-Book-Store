3c7db10d15f421a2e341ba04a34636ee8843c43a
jyoti92173 <Jyoti53913@gmail.com>
2026-08-05 22:27:21 +0530

    Create BookResponseDTO for book response

--- src/main/java/com/bookstore/onlinebookstore_service/dto/book/BookResponseDTO.java
+++ src/main/java/com/bookstore/onlinebookstore_service/dto/book/BookResponseDTO.java
@@ -1,12 +1,18 @@
 package com.bookstore.onlinebookstore_service.dto.book;
 
 
+import lombok.Data;
 
+@Data
 public class BookResponseDTO {
 
-    private Long  id;
+    private Long  bookId;
     private String title;
+
+    private Double price;
+    private Integer stock;
+
     private String author;
-    private double price;
     private String category;
+
 }
