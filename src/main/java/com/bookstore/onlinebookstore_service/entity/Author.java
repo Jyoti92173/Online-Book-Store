af745dd3d1e6b255877206fd1b0c157f9a90feae
jyoti92173 <Jyoti53913@gmail.com>
2026-08-08 22:16:08 +0530

    Create Author entity with JPA mapping

--- src/main/java/com/bookstore/onlinebookstore_service/entity/Author.java
+++ src/main/java/com/bookstore/onlinebookstore_service/entity/Author.java
@@ -1,16 +1,12 @@
 package com.bookstore.onlinebookstore_service.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "authors")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,6 +14,7 @@ import lombok.Setter;
 public class Author {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "author_id")
     private Integer authorId;
 
