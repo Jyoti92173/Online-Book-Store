a246ef7e7ede68a611f9b792c1b89be8dbdb8228
jyoti92173 <Jyoti53913@gmail.com>
2026-08-11 22:35:14 +0530

    Add Author DTOs and service layer

---
+++ src/main/java/com/bookstore/onlinebookstore_service/dto/author/AuthorRequestDTO.java
@@ -0,0 +1,16 @@
+package com.bookstore.onlinebookstore_service.dto.author;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorRequestDTO {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String authorName;
+}
