4ef27549f45633810624d59942c8c62eb50cdcdd
jyoti92173 <Jyoti53913@gmail.com>
2026-08-12 22:04:38 +0530

    Define Author service operations

--- src/main/java/com/bookstore/onlinebookstore_service/service/AuthorServiceInterface.java
+++ src/main/java/com/bookstore/onlinebookstore_service/service/AuthorServiceInterface.java
@@ -2,8 +2,6 @@ package com.bookstore.onlinebookstore_service.service;
 
 import com.bookstore.onlinebookstore_service.dto.author.AuthorRequestDTO;
 import com.bookstore.onlinebookstore_service.dto.author.AuthorResponseDTO;
-import com.bookstore.onlinebookstore_service.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
