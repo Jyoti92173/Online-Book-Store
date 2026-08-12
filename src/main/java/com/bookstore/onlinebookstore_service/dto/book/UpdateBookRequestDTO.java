8d292f646f51fbfd6bebd61b17c1925783ee38a8
jyoti92173 <Jyoti53913@gmail.com>
2026-08-12 22:20:03 +0530

    Add DTO for book update requests

--- src/main/java/com/bookstore/onlinebookstore_service/dto/book/UpdateBookRequestDTO.java
+++ src/main/java/com/bookstore/onlinebookstore_service/dto/book/UpdateBookRequestDTO.java
@@ -1,5 +1,13 @@
 package com.bookstore.onlinebookstore_service.dto.book;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class UpdateBookRequestDTO {
     private String title;
     private String author;
