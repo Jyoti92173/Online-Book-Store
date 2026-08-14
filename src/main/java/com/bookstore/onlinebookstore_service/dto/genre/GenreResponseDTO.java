6f425a101804eb6a623c938597dd9edf3668f73f
jyoti92173 <Jyoti53913@gmail.com>
2026-08-14 22:40:38 +0530

    Add GenreResponseDTO for genre details"

---
+++ src/main/java/com/bookstore/onlinebookstore_service/dto/genre/GenreResponseDTO.java
@@ -0,0 +1,13 @@
+package com.bookstore.onlinebookstore_service.dto.genre;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenreResponseDTO {
+    private Integer genreId;
+    private String genreName;
+}
