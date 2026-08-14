82affb47621edc592a5e9a282941cfa93ddc2122
jyoti92173 <Jyoti53913@gmail.com>
2026-08-14 22:41:24 +0530

    Add GenreResponseDTO for genre details"

--- src/main/java/com/bookstore/onlinebookstore_service/dto/genre/GenreResponseDTO.java
+++ src/main/java/com/bookstore/onlinebookstore_service/dto/genre/GenreResponseDTO.java
@@ -7,8 +7,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class GenreResponseDTO {
-    
+
     private Integer genreId;
     private String genreName;
 }
