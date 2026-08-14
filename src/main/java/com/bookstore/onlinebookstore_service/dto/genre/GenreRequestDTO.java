a15f93468edf4286ef393a98decce89a5905c0fe
jyoti92173 <Jyoti53913@gmail.com>
2026-08-14 22:38:49 +0530

    Add GenreRequestDTO for genre creation

---
+++ src/main/java/com/bookstore/onlinebookstore_service/dto/genre/GenreRequestDTO.java
@@ -0,0 +1,15 @@
+package com.bookstore.onlinebookstore_service.dto.genre;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenreRequestDTO {
+
+    private String genreName;
+
+}
