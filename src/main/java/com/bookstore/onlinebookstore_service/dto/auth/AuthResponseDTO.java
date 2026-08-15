dbe8ad41f6f3fd1d41c7d0d13863d21edb83821f
jyoti92173 <Jyoti53913@gmail.com>
2026-08-15 22:00:01 +0530

    Add AuthResponseDTO field

---
+++ src/main/java/com/bookstore/onlinebookstore_service/dto/auth/AuthResponseDTO.java
@@ -0,0 +1,13 @@
+package com.bookstore.onlinebookstore_service.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponseDTO {
+    private String accessToken;
+    private String tokenType;
+}
