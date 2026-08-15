375f5e2678e5b1424b3559b902ad8681fc7ba5f8
jyoti92173 <Jyoti53913@gmail.com>
2026-08-15 22:10:51 +0530

    Add RegisterRequestDTO field

---
+++ src/main/java/com/bookstore/onlinebookstore_service/dto/auth/RegisterRequestDTO.java
@@ -0,0 +1,17 @@
+package com.bookstore.onlinebookstore_service.dto.auth;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequestDTO {
+
+    private String username;
+    private String email;
+    private String password;
+    private String phone;
+}
