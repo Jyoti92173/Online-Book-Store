735eda3ad46be119376adc414f4e5f22cbfcc384
jyoti92173 <Jyoti53913@gmail.com>
2026-08-16 18:02:44 +0530

    add authentication controller for register and login

--- src/main/java/com/bookstore/onlinebookstore_service/controller/AuthController.java
+++ src/main/java/com/bookstore/onlinebookstore_service/controller/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    
     private final AuthServiceInterface authService;
 
     @PostMapping("/register")
