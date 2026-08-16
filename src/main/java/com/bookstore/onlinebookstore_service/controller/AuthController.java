705feb6d60fcb0b679fd761301864ede9bf10bb2
jyoti92173 <Jyoti53913@gmail.com>
2026-08-16 18:02:19 +0530

    add authentication controller for register and login

---
+++ src/main/java/com/bookstore/onlinebookstore_service/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.bookstore.onlinebookstore_service.controller;
+
+import com.bookstore.onlinebookstore_service.dto.auth.AuthResponseDTO;
+import com.bookstore.onlinebookstore_service.dto.auth.LoginRequestDTO;
+import com.bookstore.onlinebookstore_service.dto.auth.RegisterRequestDTO;
+import com.bookstore.onlinebookstore_service.service.AuthServiceInterface;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthServiceInterface authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDTO> register(
+            @RequestBody RegisterRequestDTO request) {
+
+        return ResponseEntity.ok(
+                authService.register(request)
+        );
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(
+            @RequestBody LoginRequestDTO request) {
+
+        return ResponseEntity.ok(
+                authService.login(request)
+        );
+    }
+
+
+
+}
