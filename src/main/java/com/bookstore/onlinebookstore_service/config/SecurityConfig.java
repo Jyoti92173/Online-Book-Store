8b708b046d44a2572ecc06741db1187941cd908f
jyoti92173 <Jyoti53913@gmail.com>
2026-09-03 22:28:20 +0530

    configure Spring Security and OAuth2 login

--- src/main/java/com/bookstore/onlinebookstore_service/config/SecurityConfig.java
+++ src/main/java/com/bookstore/onlinebookstore_service/config/SecurityConfig.java
@@ -18,12 +18,11 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http,
             CustomOAuth2SuccessHandler successHandler) throws Exception {
-
         http
                 .csrf(csrf -> csrf.disable())
 
