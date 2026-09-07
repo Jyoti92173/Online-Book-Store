61507d09f3942eb31563ee045cc40a18df999e02
jyoti92173 <Jyoti53913@gmail.com>
2026-09-07 23:30:06 +0530

    add authentication controller endpoints.

--- src/main/java/com/bookstore/onlinebookstore_service/controller/AuthController.java
+++ src/main/java/com/bookstore/onlinebookstore_service/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+
 public class AuthController {
 
     private final AuthServiceInterface authService;
