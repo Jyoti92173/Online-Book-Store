57fc90ad71653e8c461ed48faf383a3a009d7ab8
jyoti92173 <Jyoti53913@gmail.com>
2026-09-07 23:37:34 +0530

    add authentication controller endpoints.

--- src/main/java/com/bookstore/onlinebookstore_service/controller/AuthController.java
+++ src/main/java/com/bookstore/onlinebookstore_service/controller/AuthController.java
@@ -33,6 +33,7 @@ public class AuthController {
     @PostMapping("/signin")
     public ResponseEntity<AuthResponseDTO> authenticateUser(
             @RequestBody LoginRequestDTO request) {
+        
         return ResponseEntity.ok(
                 authService.login(request)
         );
