8d97405b68151259ab4bd17887214b396b28f3cb
jyoti92173 <Jyoti53913@gmail.com>
2026-09-09 22:52:55 +0530

    configure Spring Security and JWT Login

--- src/main/java/com/bookstore/onlinebookstore_service/config/SecurityConfig.java
+++ src/main/java/com/bookstore/onlinebookstore_service/config/SecurityConfig.java
@@ -119,6 +119,7 @@ public class SecurityConfig {
                         jwtAuthenticationFilter,
                         UsernamePasswordAuthenticationFilter.class
                 );
+        
         return http.build();
     }
 }
