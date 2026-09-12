07f2cb6a1c6564ae757b4511d8503b08510db420
jyoti92173 <Jyoti53913@gmail.com>
2026-09-12 23:57:54 +0530

    Add Book Controller endpoints

--- src/main/java/com/bookstore/onlinebookstore_service/controller/BookController.java
+++ src/main/java/com/bookstore/onlinebookstore_service/controller/BookController.java
@@ -48,6 +48,7 @@ public class BookController {
 
     @GetMapping("/search")
     public ResponseEntity<List<BookResponseDTO>> searchBooks(@RequestParam String title){
+        
          return ResponseEntity.ok(bookService.searchBooks(title));
     }
 }
