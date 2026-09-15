cad7836ef620a7914d0bc13f70db7260b23ec0d4
jyoti92173 <Jyoti53913@gmail.com>
2026-09-15 23:49:17 +0530

    Create User CRUD Operations.

--- src/main/java/com/bookstore/onlinebookstore_service/controller/UserController.java
+++ src/main/java/com/bookstore/onlinebookstore_service/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-public class UserController{
+public class UserController {
 
     private final UserServiceInterface userService;
 
