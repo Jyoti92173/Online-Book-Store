4fce49436d75f9f52d4407785a58dbee6c67b0ab
jyoti92173 <Jyoti53913@gmail.com>
2026-09-16 22:49:23 +0530

    implement user profile retrieval and update in UserController

--- src/main/java/com/bookstore/onlinebookstore_service/controller/UserController.java
+++ src/main/java/com/bookstore/onlinebookstore_service/controller/UserController.java
@@ -19,7 +19,6 @@ public class UserController {
 
 
 
-
     
     @GetMapping(value = "/profile",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserResponseDTO> getProfile() {
