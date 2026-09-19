5d90e9a248c8149218842f86ea5a4b9e6e16690b
jyoti92173 <Jyoti53913@gmail.com>
2026-09-19 22:31:16 +0530

    implement user registration and login authentication

--- src/main/java/com/bookstore/onlinebookstore_service/service/impl/AuthServiceImpl.java
+++ src/main/java/com/bookstore/onlinebookstore_service/service/impl/AuthServiceImpl.java
@@ -65,7 +65,7 @@ public class AuthServiceImpl implements AuthServiceInterface {
         String refreshToken = jwtUtil.generateRefreshToken(user.getEmail());
 
         AuthResponseDTO response = new AuthResponseDTO();
-        
+
         response.setAccessToken(accessToken);
         response.setRefreshToken(refreshToken);
         response.setTokenType("Bearer");
