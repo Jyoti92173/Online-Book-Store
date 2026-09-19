1dbbc176a5f201b0ad25e666f88a40638db3f5e9
jyoti92173 <Jyoti53913@gmail.com>
2026-09-19 22:21:21 +0530

    implement user registration and login authentication

--- src/main/java/com/bookstore/onlinebookstore_service/service/impl/AuthServiceImpl.java
+++ src/main/java/com/bookstore/onlinebookstore_service/service/impl/AuthServiceImpl.java
@@ -2,31 +2,35 @@ package com.bookstore.onlinebookstore_service.service.impl;
 
 import com.bookstore.onlinebookstore_service.dto.auth.AuthResponseDTO;
 import com.bookstore.onlinebookstore_service.dto.auth.LoginRequestDTO;
+import com.bookstore.onlinebookstore_service.dto.auth.RefreshTokenRequestDTO;
 import com.bookstore.onlinebookstore_service.dto.auth.RegisterRequestDTO;
 import com.bookstore.onlinebookstore_service.entity.User;
 import com.bookstore.onlinebookstore_service.enums.Role;
 import com.bookstore.onlinebookstore_service.repository.UserRepository;
+import com.bookstore.onlinebookstore_service.security.JwtUtil;
 import com.bookstore.onlinebookstore_service.service.AuthServiceInterface;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+
 @Service
 @RequiredArgsConstructor
-
 public class AuthServiceImpl implements AuthServiceInterface {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     @Override
     public AuthResponseDTO register(RegisterRequestDTO request) {
 
         if(userRepository.existsByEmail(request.getEmail())) {
-            throw new UsernameNotFoundException("Email Already Exists");
+            throw new IllegalArgumentException("Email already exists");
         }
 
         User user = new User();
@@ -45,8 +49,8 @@ public class AuthServiceImpl implements AuthServiceInterface {
     }
 
     @Override
-    public AuthResponseDTO login(LoginRequestDTO request) {
-        
+    public AuthResponseDTO login (LoginRequestDTO request) {
+
         User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() ->
                         new RuntimeException("Invalid email or password"));
@@ -58,10 +62,23 @@ public class AuthServiceImpl implements AuthServiceInterface {
             throw new RuntimeException("Invalid email or password");
         }
 
+        String accessToken = jwtUtil.generateAccessToken(user.getEmail());
+        String refreshToken = jwtUtil.generateRefreshToken(user.getEmail());
+
         AuthResponseDTO response = new AuthResponseDTO();
 
+        response.setAccessToken(accessToken);
+        response.setRefreshToken(refreshToken);
+        response.setTokenType("Bearer");
         response.setMessage("Login successful");
 
         return response;
     }
+
+    @Override
+    public AuthResponseDTO refresh(RefreshTokenRequestDTO request) {
+        return null;
+    }
+
+
 }
