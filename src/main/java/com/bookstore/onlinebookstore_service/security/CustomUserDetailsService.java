2b6a386136ccff376618a4197f97751fcc45417b
jyoti92173 <Jyoti53913@gmail.com>
2026-09-22 23:28:09 +0530

    implement custom user details service for authentication

--- src/main/java/com/bookstore/onlinebookstore_service/security/CustomUserDetailsService.java
+++ src/main/java/com/bookstore/onlinebookstore_service/security/CustomUserDetailsService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailsService implements UserDetailsService {
 
 
-
     
     private final UserRepository userRepository;
 
