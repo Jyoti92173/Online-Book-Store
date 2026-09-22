1e2951740b68ec4d02904b1f857bb7a64c76d15f
jyoti92173 <Jyoti53913@gmail.com>
2026-09-22 23:24:15 +0530

    implement custom user details service for authentication

---
+++ src/main/java/com/bookstore/onlinebookstore_service/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.bookstore.onlinebookstore_service.security;
+
+import com.bookstore.onlinebookstore_service.entity.User;
+import com.bookstore.onlinebookstore_service.repository.UserRepository;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email" + email));
+
+        return org.springframework.security.core.userdetails.User.withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
