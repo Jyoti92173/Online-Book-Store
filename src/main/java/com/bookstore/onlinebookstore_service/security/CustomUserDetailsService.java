f06f32ae44d3e9f982af343c5613b42167f93404
jyoti92173 <Jyoti53913@gmail.com>
2026-09-22 23:24:34 +0530

    implement custom user details service for authentication

--- src/main/java/com/bookstore/onlinebookstore_service/security/CustomUserDetailsService.java
+++ src/main/java/com/bookstore/onlinebookstore_service/security/CustomUserDetailsService.java
@@ -17,7 +17,6 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
-
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
